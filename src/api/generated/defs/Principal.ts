66aa4c50baef076881df5793a474920f08adf6dd
Wlazrad <radek.wlazly@gmail.com>
2020-02-03 18:56:42 +0100

    add cardslist component and cards module

---
+++ src/api/generated/defs/Principal.ts
@@ -0,0 +1,14 @@
+/* tslint:disable:max-line-length */
+/**
+ * Api Documentation
+ * 1.0
+ * Api Documentation
+ * urn:tos
+ * Apache 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * localhost:8080
+ */
+
+export interface Principal {
+  name?: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import {HttpClientModule} from '@angular/common/http';
 import {HttpService} from './http.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { CardsListComponent } from './cards/cards-list/cards-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardsListComponent
   ],
   imports: [
     BrowserModule,
